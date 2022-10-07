1819ce831b33601765cac5588cc542b95cd6f11b
Hajni83 <maro.hajnalka@gmail.com>
2022-10-07 09:34:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from "react";
+import TerminatorList from "./components/terminator-list/terminator-list.component";
+import SearchBox from "./components/searchBox/searchBox.component";
+import './App.css';
+
+function App () {
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         models:[],
+    //         searchfield:''
+    //     }
+    // }
+
+        const [models, setModels] = useState([]);
+        const [searchfield, setSearchfield] = useState('');
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
+    };
+
+    useEffect(() =>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then ((users) => setModels(users))
+    }, [])
+
+    
+    {
+        const filteredModels =models.filter((model) => {
+            return model.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+
+        if(models.length === 0){
+            return <h1>Betöltés folyamatban ...</h1>
+        }
+
+        return (
+            <div className="tc">
+                <h1 className="f1">Terminator modellek</h1>
+                <SearchBox searchChange = {onSearchChange}/>
+                <TerminatorList models = {filteredModels}/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "tachyons";
+import App from './App';
+import SearchBox from './components/searchBox/searchBox.component';
+
+ReactDOM.render(
+ <App/>,
+  document.getElementById("root")
+);
